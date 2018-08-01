723cd299083d639b29c0b676904749b9228d1f4b
BobdeTheije <bob@theije.com>
2018-08-01 15:02:50 +0200

    Add files via upload

--- belle.R
+++ belle.R
@@ -11,7 +11,10 @@ g <- ggplot(belle, aes(Datum)) +
     geom_line(aes(y = half_nw, colour = "half_nw")) +
     geom_line(aes(y = heel_nw, colour = "heel_nw")) + 
     geom_hline(yintercept = 50, colour = "green") + 
-    scale_x_date(date_breaks = "1 week", date_labels = "%b %d")
+    geom_hline(yintercept = 46, colour = "yellow") + 
+    geom_hline(yintercept = 45, colour = "dark grey") + 
+    scale_x_date(date_breaks = "1 week", date_labels = "%b %d") +
+    scale_y_continuous(breaks=c(39,40,41,42,43,44,45,46,47,48,49,50))
 
 print(g)
 
